884eff3145555e8b4ce3c7be534cc56eec90d2e4
JUAN manuel calleros <116044083+manuelcalso@users.noreply.github.com>
2024-01-20 11:50:07 -0600

    pruebas bottones y layouts

--- app/juan/page.tsx
+++ app/juan/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+
 
 export default function JuanTest() {
   return (
+    <>
     <div>Hola soy juan</div>
+    <Button variant="destructive">Este es el button</Button>
+    </>
   )
 }
